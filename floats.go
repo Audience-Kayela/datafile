f8cd0abdda636944663ee23677bd0dc8bb0e517e
Audience-Kayela <akayela22@gmail.com>
2024-01-31 07:04:58 +0200

    retruning nil slice

--- floats.go
+++ floats.go
@@ -3,32 +3,31 @@ package datafile
 
 import (
 	"bufio"
-	"strconv"
 	"os"
+	"strconv"
 )
 
 // GetFloats reads a float64 from each line of a file.
-func GetFloats(fileName string) ([] float64, error) {
-	var numbers [] float64
+func GetFloats(fileName string) ([]float64, error) {
+	var numbers []float64
 	file, err := os.Open(fileName)
 	if err != nil {
-		return numbers, err
+		return nil, err
 	}
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		number, err := strconv.ParseFloat(scanner.Text(), 64)
 		if err != nil {
-			return numbers, err
+			return nil, err
 		}
 		numbers = append(numbers, number)
 	}
 	err = file.Close()
 	if err != nil {
-		return numbers, err
+		return nil, err
 	}
 	if scanner.Err() != nil {
-		return numbers, scanner.Err()
+		return nil, scanner.Err()
 	}
 	return numbers, nil
 }
-	
